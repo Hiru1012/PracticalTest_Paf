f5ed16defce47595cb28cca4a70bc20032217fb1
Hiru1012 <hiruniyatagama@gmail.com>
2020-05-06 12:34:19 +0530

    modify the DB

--- DB/hcs.sql
+++ DB/hcs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2020 at 08:56 AM
+-- Generation Time: May 06, 2020 at 09:01 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
